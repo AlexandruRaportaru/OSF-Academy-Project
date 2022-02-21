ef3deb788ffe8f488d099cd6022a17643f8fa558
Alex <alecs_claudiu@yahoo.com>
2022-02-21 23:12:45 +0200

    Add Cookies pop-up + login modal

--- JS/Home.js
+++ JS/Home.js
@@ -19,4 +19,67 @@ const loadmore = document.querySelector('.loadMore');
     })
 
 
+// COOKIES POPUP //
 
+$(document).ready(function() { 
+    var cookie = false;
+    var cookieContent = $('.cookie-disclaimer');
+
+    checkCookie();
+
+    if (cookie === true) {
+      cookieContent.hide();
+    }
+
+    function setCookie(cname, cvalue, exdays) {
+      var d = new Date();
+      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+      var expires = "expires=" + d.toGMTString();
+      document.cookie = cname + "=" + cvalue + "; " + expires;
+    }
+
+    function getCookie(cname) {
+      var name = cname + "=";
+      var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+      }
+      return "";
+    }
+
+    function checkCookie() {
+      var check = getCookie("acookie");
+      if (check !== "") {
+        return cookie = true;
+      } else {
+          return cookie = false; //setCookie("acookie", "accepted", 365);
+      }
+      
+    }
+    $('.accept-cookie').click(function () {
+      setCookie("acookie", "accepted", 365);
+      cookieContent.hide(500);
+    });
+});
+
+
+// PASSWORD VIEW IN MODAL //
+
+const togglePassword = document.querySelector("#togglePassword");
+        const password = document.querySelector("#password");
+
+        togglePassword.addEventListener("click", function () {
+            // toggle the type attribute
+            const type = password.getAttribute("type") === "password" ? "text" : "password";
+            password.setAttribute("type", type);
+            
+            // toggle the icon
+            this.classList.toggle("bi-eye");
+        });
+
+        // prevent form submit
+        const form = document.querySelector("form");
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+        });
